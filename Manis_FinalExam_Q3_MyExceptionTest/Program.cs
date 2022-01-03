0f4804c6b04ad39e3e4710f1abe0ff5d56d1ac29
ethan3212 <74456094+ethan3212@users.noreply.github.com>
2022-01-03 09:00:14 -0600

    Add files via upload

---
+++ Manis_FinalExam_Q3_MyExceptionTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace Manis_FinalExam_Q3_MyExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] theArray = { 1.11, 2.22, 3.33, 4.44, 5.55, 6.66, 7.77, 8.88, 9.99, 10.10 };
+            int number;
+            
+            for (int i = 0; i < theArray.Length; i++)
+            {
+                try
+                {
+                    WriteLine("Enter an index value of the array (0 - 9): ");
+                    number = int.Parse(ReadLine());
+                    if (number < 0 || number >= 10)
+                        throw (new ApplicationException("Entered index does not exist. Press any key to continue: "));
+                    WriteLine("The number at index {0}, is {1}: ", number, theArray[number]);
+                    WriteLine(" ");
+                }
+                catch (ApplicationException e)
+                {
+
+                    WriteLine("Error: {0}", e.Message);
+                    WriteLine(" ");
+                    ReadLine();
+                }
+            }
+        }
+    }
+}
